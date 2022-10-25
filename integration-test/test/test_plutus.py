f39be5daa6acc33a59d68fb4ca153394efcbbd95
Mateus Santos <santos.mateus@protonmail.com>
2022-10-25 18:56:09 -0300

    fixed fund project function

--- integration-test/test/test_plutus.py
+++ integration-test/test/test_plutus.py
@@ -10,12 +10,11 @@ from .base import TEST_RETRIES, TestBase
 
 
 class TestPlutus(TestBase):
-
     @retry(tries=TEST_RETRIES, backoff=1.5, delay=3, jitter=(0, 4))
     def test_create_user_stake_transaction(self):
         sys.path.append("../src")
 
-        from lib import script_tools
+        from lib import script_tools, cardano_types
 
         sender_address = Address(self.payment_vkey.hash(), network=self.NETWORK)
         nft_symbol = (
@@ -25,6 +24,14 @@ class TestPlutus(TestBase):
         )
 
         # ----------- Mint Script NFT -----------
+
+        tokens_policy = (
+            (
+                ScriptPubkey(VerificationKey.from_signing_key(self.payment_skey).hash())
+                .hash()
+                .payload
+            ),
+        )
 
         script_tools.mint_nfts(
             self.chain_context,
@@ -42,7 +49,7 @@ class TestPlutus(TestBase):
                 address=sender_address,
                 amount=Value.from_primitive(
                     [
-                        3_000_000,
+                        5_000_000,
                         {nft_symbol: {b"mediator": 3, b"target": 2}},
                     ]
                 ),
@@ -51,59 +58,55 @@ class TestPlutus(TestBase):
 
         # ----------- Giver give ---------------
 
-        # with open("../script/script.plutus", "r") as f:
-        #     script_hex = f.read()
+        with open("../script/script.plutus", "r") as f:
+            script_hex = f.read()
 
-        #     transaction = cardano_utils.create_user_stake_transaction(
-        #         self.chain_context,
-        #         sender_address,
-        #         script_hex,
-        #         ScriptPubkey(VerificationKey.from_signing_key(self.payment_skey).hash())
-        #         .hash()
-        #         .payload.hex(),
-        #     )
+            transaction = script_tools.create_transaction_fund_project(
+                self.chain_context,
+                sender_address,
+                10_000_000,
+                script_hex,
+                tokens_policy,
+                tokens_policy,
+                tokens_policy,
+                1691711382000,
+            )
 
-        #     gero_script = cbor2.loads(bytes.fromhex(script_hex))
-        #     script_hash = plutus_script_hash(PlutusV1Script(gero_script))
-        #     script_address = Address(script_hash, network=self.NETWORK)
+            escrow_script = cbor2.loads(bytes.fromhex(script_hex))
+            script_hash = plutus_script_hash(PlutusV1Script(escrow_script))
+            script_address = Address(script_hash, network=self.NETWORK)
 
-        # # ----------- Submit Transaction --------------
+        # ----------- Submit Transaction --------------
 
-        # # Sign the transaction body hash
-        # signature = self.payment_skey.sign(transaction.transaction_body.hash())
+        # Sign the transaction body hash
+        signature = self.payment_skey.sign(transaction.transaction_body.hash())
 
-        # # Add verification key and the signature to the witness set
-        # vk_witnesses = [VerificationKeyWitness(self.payment_vkey, signature)]
+        # Add verification key and the signature to the witness set
+        vk_witnesses = [VerificationKeyWitness(self.payment_vkey, signature)]
 
-        # transaction.transaction_witness_set = TransactionWitnessSet(
-        #     vkey_witnesses=vk_witnesses
-        # )
+        transaction.transaction_witness_set = TransactionWitnessSet(
+            vkey_witnesses=vk_witnesses
+        )
 
-        # signed_tx = Transaction(
-        #     transaction.transaction_body,
-        #     TransactionWitnessSet(vkey_witnesses=vk_witnesses),
-        # )
+        signed_tx = Transaction(
+            transaction.transaction_body,
+            TransactionWitnessSet(vkey_witnesses=vk_witnesses),
+        )
 
-        # self.chain_context.submit_tx(signed_tx.to_cbor())
+        self.chain_context.submit_tx(signed_tx.to_cbor())
 
-        # time.sleep(3)
+        time.sleep(3)
 
-        # self.assert_output(
-        #     script_address,
-        #     TransactionOutput(
-        #         address=script_address,
-        #         amount=Value.from_primitive(
-        #             [
-        #                 3_000_000,
-        #                 {nft_symbol: {b"us-0": 1}},
-        #             ]
-        #         ),
-        #         datum_hash=cardano_types.UserStakeDetail(
-        #             user_pkh=sender_address.payment_part.payload,
-        #             votes={},
-        #             delegated_to=sender_address.payment_part.payload,
-        #             index=0,
-        #             sym=nft_symbol,
-        #         ).hash(),
-        #     ),
-        # )
+        self.assert_output(
+            script_address,
+            TransactionOutput(
+                address=script_address,
+                amount=10_000_000,
+                datum_hash=cardano_types.ContractDatum(
+                    mediators=tokens_policy,
+                    target=tokens_policy,
+                    fallback=tokens_policy,
+                    deadline=1691711382000,
+                ).hash(),
+            ),
+        )
